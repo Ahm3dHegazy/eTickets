d0e0eecd6e0408e2461ee6e3b3d1f38cb5b987f4
ahmed 7egazy <abdelsamiea943@gmail.com>
2026-08-08 19:17:03 +0300

    add AppDbContext file

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Cinema> cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
