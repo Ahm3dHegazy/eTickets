8fe66b4f631e7c465b88b9143e7f1b4344b29950
ahmed 7egazy <abdelsamiea943@gmail.com>
2026-08-10 10:38:22 +0300

    add actorsService

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        void Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
