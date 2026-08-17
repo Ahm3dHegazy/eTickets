0f8a21c67e7e99c78653aaa3178f6b6a1b3e5f6f
ahmed 7egazy <abdelsamiea943@gmail.com>
2026-08-18 00:42:54 +0300

    feat(session): adding SessionExtensions

---
+++ eTickets.Data/Extensions/SessionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+
+namespace eTickets.Data.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetString(this ISession session, string key, string value)
+        {
+            session.Set(key, Encoding.UTF8.GetBytes(value));
+        }
+
+        public static string? GetString(this ISession session, string key)
+        {
+            return session.TryGetValue(key, out var data)
+                ? Encoding.UTF8.GetString(data)
+                : null;
+        }
+    }
+}
