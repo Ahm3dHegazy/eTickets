6afc80bcdf4a6a9ec8629ec05b9221ac0db0d3b6
ahmed 7egazy <abdelsamiea943@gmail.com>
2026-08-18 00:34:35 +0300

    feat(ShoppingCart): adding ShoppingCartItem

---
+++ eTickets.Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string MovieImageURL { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice => Price * Quantity;
+    }
+}
